5a47beb206ba79acbcc2556980b96e247cada4da
Maksim Nosov <ozolc@yandex.ru>
2019-03-11 15:40:10 +0300

    Main preparations

---
+++ courses/21-Networking/09-Refactoring/Networking/CollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  CollectionViewCell.swift
+//  Networking
+//
+//  Created by Maksim Nosov on 11/03/2019.
+//  Copyright © 2019 Alexey Efimov. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+   
+    @IBOutlet weak var label: UILabel!
+    
+}
