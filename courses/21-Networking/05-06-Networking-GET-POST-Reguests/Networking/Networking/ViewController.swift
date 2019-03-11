b6b31de2fa26bf585ace639a527d18d079732f26
Maksim Nosov <ozolc@yandex.ru>
2019-03-11 11:43:54 +0300

    Рассмотрен GET запрос

---
+++ courses/21-Networking/05-06-Networking-GET-POST-Reguests/Networking/Networking/ViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ViewController.swift
+//  Networking
+//
+//  Created by Alexey Efimov on 25/07/2018.
+//  Copyright © 2018 Alexey Efimov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBAction func getRequest(_ sender: Any) {
+        
+        guard let url = URL(string: "https://jsonplaceholder.typicode.com/posts") else { return }
+        
+        let session = URLSession.shared
+        session.dataTask(with: url) { (data, response, error) in
+            
+            guard
+                let response = response, let data = data else { return }
+            
+            print(response)
+            print(data)
+            
+            do {
+                let json = try JSONSerialization.jsonObject(with: data, options: [])
+                print(json)
+            } catch {
+                print(error)
+            }
+            
+        }.resume()
+    }
+    
+    @IBAction func postRequest(_ sender: Any) {
+    }
+    
+}
+
