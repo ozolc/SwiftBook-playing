6ee5e4024ff18fe46b727e8c4b770ac9ee14f531
Maksim Nosov <ozolc@yandex.ru>
2019-03-11 12:39:40 +0300

    Initial commit

---
+++ courses/21-Networking/07-Get-And-Parse-Data/Networking/Course.swift
@@ -0,0 +1,19 @@
+//
+//  Course.swift
+//  Networking
+//
+//  Created by Maksim Nosov on 11/03/2019.
+//  Copyright © 2019 Alexey Efimov. All rights reserved.
+//
+
+import Foundation
+
+struct Course {
+    
+    let id: Int
+    let name: String
+    let link: String
+    let imageUrl: String
+    let number_of_lessons: Int
+    let number_of_tests: Int
+}
---
+++ courses/21-Networking/07-Get-And-Parse-Data/Networking/CoursesViewController.swift
@@ -0,0 +1,58 @@
+//
+//  CoursesViewController.swift
+//  Networking
+//
+//  Created by Alexey Efimov on 06.09.2018.
+//  Copyright © 2018 Alexey Efimov. All rights reserved.
+//
+
+import UIKit
+
+class CoursesViewController: UIViewController {
+
+    @IBOutlet var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
+
+// MARK: Table View Data Source
+
+extension CoursesViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell") as! TableViewCell
+        
+        return cell
+    }
+}
+
+// MARK: Table View Delegate
+
+extension CoursesViewController: UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 100
+    }
+}
+
