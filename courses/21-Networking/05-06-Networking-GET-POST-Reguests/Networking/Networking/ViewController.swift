da0c755c026202b998e69dbc5f189dea7c7237ee
Maksim Nosov <ozolc@yandex.ru>
2019-03-11 12:27:07 +0300

    Реализация POST запроса

--- courses/21-Networking/05-06-Networking-GET-POST-Reguests/Networking/Networking/ViewController.swift
+++ courses/21-Networking/05-06-Networking-GET-POST-Reguests/Networking/Networking/ViewController.swift
@@ -34,6 +34,34 @@ class ViewController: UIViewController {
     }
     
     @IBAction func postRequest(_ sender: Any) {
+        
+        guard let url = URL(string: "https://jsonplaceholder.typicode.com/posts") else { return }
+        
+        let userData = ["Course": "Networking", "Lesson": "GET and POST Request"]
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        
+        guard let httpBody = try? JSONSerialization.data(withJSONObject: userData, options: []) else { return }
+        request.httpBody = httpBody
+        
+        let session = URLSession.shared
+        session.dataTask(with: request) { (data, response, error) in
+            
+            guard
+                let response = response, let data = data else { return }
+            
+            print(response)
+            
+            do {
+                let json = try JSONSerialization.jsonObject(with: data, options: [])
+                print(json)
+            } catch {
+                print(error)
+            }
+        }.resume()
+        
     }
     
 }
