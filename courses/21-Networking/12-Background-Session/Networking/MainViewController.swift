684b981366ef9f6401a33a83363e664c5fc54bf8
Maksim Nosov <ozolc@yandex.ru>
2019-03-12 09:39:51 +0300

    Realization showAlert with ProgressBar and ActivityIndicator

--- courses/21-Networking/12-Background-Session/Networking/MainViewController.swift
+++ courses/21-Networking/12-Background-Session/Networking/MainViewController.swift
@@ -26,6 +26,44 @@ private let uploadImage = "https://api.imgur.com/3/image"
 class MainViewController: UICollectionViewController {
     
     private let actions = Actions.allCases
+    private var alert: UIAlertController!
+    
+    private func showAlert() {
+        
+        alert = UIAlertController(title: "Downloading ...", message: "0%", preferredStyle: .alert)
+        
+        let heght = NSLayoutConstraint(item: alert.view,
+                                       attribute: .height,
+                                       relatedBy: .equal,
+                                       toItem: nil,
+                                       attribute: .notAnAttribute,
+                                       multiplier: 0,
+                                       constant: 170)
+        alert.view.addConstraint(heght)
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .destructive, handler: nil)
+        
+        alert.addAction(cancelAction)
+        present(alert, animated: true) {
+            
+            let size = CGSize(width: 40, height: 40)
+            let point = CGPoint(x: self.alert.view.frame.width / 2 - size.width / 2, y: self.alert.view.frame.height / 2 - size.height / 2)
+            
+            let activityIndicator = UIActivityIndicatorView(frame: CGRect(origin: point, size: size))
+            activityIndicator.color = .gray
+            activityIndicator.startAnimating()
+            
+            let progressView = UIProgressView(frame: CGRect(x: 0, y: self.alert.view.frame.height - 44, width: self.alert.view.frame.width, height: 2))
+            
+            progressView.tintColor = .blue
+            progressView.progress = 0.5
+            
+            self.alert.view.addSubview(activityIndicator)
+            self.alert.view.addSubview(progressView)
+            
+        }
+        
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -64,6 +102,7 @@ class MainViewController: UICollectionViewController {
         case .uploadImage:
             NetworkManager.uploadImage(url: uploadImage)
         case .downloadFile:
+            showAlert()
             print(action.rawValue)
         }
     }
