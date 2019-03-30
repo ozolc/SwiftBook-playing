20f9ca185b72c4205e63a9491498d1abfed4d386
Maksim Nosov <ozolc@yandex.ru>
2019-03-30 10:23:07 +0300

    9 Course 7 Problem app

--- courses/08-GCD/GCD/GCD/SecondViewController.swift
+++ courses/08-GCD/GCD/GCD/SecondViewController.swift
@@ -10,7 +10,34 @@ import UIKit
 
 class SecondViewController: UIViewController {
     
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    
+    fileprivate var imageURL: URL?
+    fileprivate var image: UIImage? {
+        get {
+            return imageView.image
+        }
+        
+        set {
+            activityIndicator.stopAnimating()
+            activityIndicator.isHidden = true
+            imageView.image = newValue
+            imageView.sizeToFit()
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        fetchImage()
     }
+    
+    fileprivate func fetchImage() {
+        imageURL = URL(string: "https://upload.wikimedia.org/wikipedia/commons/0/07/Huge_ball_at_Vilnius_center.jpg")
+        activityIndicator.startAnimating()
+        activityIndicator.isHidden = false
+        guard let url = imageURL, let imageData = try? Data(contentsOf: url) else { return }
+        self.image = UIImage(data: imageData)
+    }
+    
 }
