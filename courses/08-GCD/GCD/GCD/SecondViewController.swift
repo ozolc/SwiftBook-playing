0434c0ee23cfb6dc295d175c1274e3bb3c6a9e50
Maksim Nosov <ozolc@yandex.ru>
2019-03-30 10:07:16 +0300

    9 Course 6 App interface

---
+++ courses/08-GCD/GCD/GCD/SecondViewController.swift
@@ -0,0 +1,16 @@
+//
+//  SecondViewController.swift
+//  GCD
+//
+//  Created by Maksim Nosov on 30/03/2019.
+//  Copyright © 2019 Maksim Nosov. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
---
+++ courses/08-GCD/GCD/GCD/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GCD
+//
+//  Created by Maksim Nosov on 30/03/2019.
+//  Copyright © 2019 Maksim Nosov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
