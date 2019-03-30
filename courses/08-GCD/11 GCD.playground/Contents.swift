90fde5449e0a154bd8974f3ee61b05fcaf36ab66
Maksim Nosov <ozolc@yandex.ru>
2019-03-30 11:44:17 +0300

    9 Course 11 Barrier practice

---
+++ courses/08-GCD/11 GCD.playground/Contents.swift
@@ -0,0 +1,39 @@
+import UIKit
+import PlaygroundSupport
+
+PlaygroundPage.current.needsIndefiniteExecution = true
+
+class SafeArray<Element> {
+    private var array = [Element]()
+    private let queue = DispatchQueue.init(label: "DispatchBarrier", attributes: .concurrent)
+//    , attributes: .concurrent
+    public func append(element: Element) {
+        queue.async() {
+            print(Thread.current)
+            self.array.append(element)
+        }
+    }
+    
+    public var elements: [Element] {
+        var result = [Element]()
+        queue.sync {
+            result = self.array
+        }
+        
+        return result
+    }
+    
+}
+
+var safeArray = SafeArray<Int>()
+DispatchQueue.concurrentPerform(iterations: 10) { (index) in
+    safeArray.append(element: index)
+}
+print("array: ", safeArray.elements)
+
+
+//var array = [Int]()
+//DispatchQueue.concurrentPerform(iterations: 10) { (index) in
+//    array.append(index)
+//}
+//print("array: ", array)
